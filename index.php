f241d3dcd758bb8445d679c21d94724041d91afc
Robert <rdtheriault@yahoo.com>
2018-12-17 11:29:27 -0800

    Update index.php
    Fixed "Cannot modify header information - headers already sent by" error

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+	$str = "$_SERVER[REQUEST_URI]";  //$_SERVER[HTTP_HOST]
+	$last = explode("?",$str);
+	$url = $last[1];
+
+	$myfile = fopen("url.txt", "r") or die("Unable to open file!");
+	// Output one line until end-of-file
+	while(!feof($myfile)) {
+
+		$test = fgets($myfile);
+		$test2 = explode(" ",$test);
+		if ($test2[0] == $url){
+			//fclose($myfile);
+			header( 'Location: '.$test2[1] ) ;
+			echo "<script> window.location.replace('".$test2[1]."');</script>";
+		}
+	}
+	fclose($myfile);
+	echo "I'm sorry, that link seems to not work or you did not add one or you have headers and Javscript turned of or...";
+?>
 <html>
 	<head>
 		
